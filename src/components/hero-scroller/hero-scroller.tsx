2e867ec8f43b5bdfd55fb601a9aa198c3a05c987
Elora <56364111+eloraa@users.noreply.github.com>
2026-06-24 17:49:15 +0100

    feat: make hero section responsive

--- src/components/hero-scroller/hero-scroller.tsx
+++ src/components/hero-scroller/hero-scroller.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link';
 
 export const HeroScroller = () => {
   return (
-    <div className="flex items-center h-60 mt-16 overflow-hidden">
-      <div className="flex relative h-24 w-full">
+    <div className="flex items-center h-40 md:h-60 md:mt-16 overflow-hidden">
+      <div className="flex relative h-16 md:h-24 w-full">
         <div className="absolute h-full w-[calc(100%+2rem)] bg-primary-600 -rotate-3 -mx-4"></div>
         <div className="h-full w-[calc(100%+2rem)] bg-secondary-500 relative z-1 flex items-center rotate-3 -mx-4">
           <Marquee config={{ speed: 60 }}>
             <div className="flex items-center gap-6 py-2 h-full text-white">
               {Array.from({ length: 6 }).map((_, i) => (
                 <Fragment key={i}>
-                  <Link href="#" className="text-5xl font-semibold uppercase">
+                  <Link href="#" className="text-3xl md:text-5xl font-semibold uppercase">
                     Let's Talk
                   </Link>
                   <span>
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -8,9 +8,9 @@ export const Hero = () => {
   return (
     <div className="container py-18 mt-18">
       <div className="flex items-center justify-center">
-        <div className="text-center max-w-3xl mx-auto">
-          <h1 className="font-bold text-6xl text-secondary-500 leading-[130%]">Engineering Intelligent Technology For The Future</h1>
-          <p className="mt-6">
+        <div className="text-center md:max-w-3xl mx-auto">
+          <h1 className="font-bold text-2xl md:text-6xl text-secondary-500 leading-[130%]">Engineering Intelligent Technology For The Future</h1>
+          <p className="max-md:text-xs mt-3 md:mt-6">
             LevelAxis is a global technology partner delivering AI-powered solutions, custom software, and digital transformation strategies that help businesses innovate faster and operate smarter.
           </p>
         </div>
@@ -20,27 +20,27 @@ export const Hero = () => {
         <div className="grid lg:grid-cols-3 gap-5">
           {/* Right */}
           <div className="grid gap-5">
-            <div className="h-86">
+            <div className="h-50 lg:h-86">
               <Image src={banner1img} alt="Banner Image" className="size-full object-cover" />
             </div>
-            <div className="flex gap-5 h-52">
+            <div className="flex gap-5 h-40 md:h-52">
               <div className="size-full bg-secondary-500 p-4 text-white flex flex-col justify-between">
                 <div className="flex items-center">
-                  <h1 className="font-medium text-5xl">
+                  <h1 className="font-medium text-3xl md:text-5xl">
                     50<span className="sr-only">+</span>
                   </h1>
                   <PlusIcon className="size-10" strokeWidth={2.5} />
                 </div>
                 <div>
-                  <p className="font-medium text-sm">Successful Projects</p>
+                  <p className="font-medium text-xs md:text-sm">Successful Projects</p>
                 </div>
               </div>
               <div className="size-full bg-primary-600 p-4 text-white flex flex-col justify-between">
                 <div>
-                  <p className="font-medium text-sm">Industries Served</p>
+                  <p className="font-medium text-xs md:text-sm">Industries Served</p>
                 </div>
                 <div className="flex items-center">
-                  <h1 className="font-medium text-5xl">
+                  <h1 className="font-medium text-3xl md:text-5xl">
                     10<span className="sr-only">+</span>
                   </h1>
                   <PlusIcon className="size-10" strokeWidth={2.5} />
@@ -49,33 +49,33 @@ export const Hero = () => {
             </div>
           </div>
           {/* Middle */}
-          <div className="max-lg:h-86">
+          <div className="max-lg:h-50">
             <Image src={banner2img} alt="Banner Image" className="size-full object-cover" />
           </div>
           {/* Left */}
           <div className="grid gap-5">
-            <div className="flex gap-5 h-52">
+            <div className="flex gap-5 h-40 md:h-52">
               <div className="size-full bg-primary-600 p-4 text-white flex flex-col justify-between">
                 <div className="flex items-center">
-                  <h1 className="font-medium text-5xl">
+                  <h1 className="font-medium text-3xl md:text-5xl">
                     20<span className="sr-only">+</span>
                   </h1>
                   <PlusIcon className="size-10" strokeWidth={2.5} />
                 </div>
                 <div>
-                  <p className="font-medium text-sm">Technology Experts</p>
+                  <p className="font-medium text-xs md:text-sm">Technology Experts</p>
                 </div>
               </div>
               <div className="size-full bg-secondary-500 p-4 text-white flex flex-col justify-between">
                 <div>
-                  <p className="font-medium text-sm">Client Satisfaction</p>
+                  <p className="font-medium text-xs md:text-sm">Client Satisfaction</p>
                 </div>
                 <div className="flex items-center">
-                  <h1 className="font-medium text-5xl">99%</h1>
+                  <h1 className="font-medium text-3xl md:text-5xl">99%</h1>
                 </div>
               </div>
             </div>
-            <div className="h-86">
+            <div className="h-50 lg:h-86">
               <Image src={banner1img} alt="Banner Image" className="size-full object-cover" />
             </div>
           </div>
